ff18b442a02b31585d34f710100b91d227c765b7
MSI75 <sisaif56@gmail.com>
2022-12-24 23:12:49 +0600

    Security system

--- system.cpp
+++ system.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main(){
     int a, i = 0;
     string text, old, password1, password2, pass, name, password0, age, user, word, word1;
-    string cards[2], cp[2];
+    string creds[2], cp[2];
 
     cout<<"                Security system                      "<<endl;
     cout<<"_____________________________________________________"<<endl<<endl;
@@ -43,9 +43,110 @@ int main(){
                 }
                  break;
             }
-           
+            case 2:{
+                i = 0;
+                cout<<"__________________________________"<<endl<<endl;
+                cout<<"|-------------Login--------------|"<<endl;
+                cout<<"|________________________________|"<<endl<<endl;
+
+                ifstream of2;
+                of2.open("file.txt");
+                cout<<"Please enter the userName :- ";
+                cin>>user;
+                cout<<"Please enter the password :- ";
+                cin>>pass;
+
+                if(of2.is_open()){
+                    while(!of2.eof()){
+                        while (getline(of2, text))
+                        {
+                            istringstream iss(text);
+                            iss>>word;
+
+                            creds[i]=word;
+                            i++;
+                        }
+
+                       if(user==creds[0]&&pass==creds[1]){
+                        cout<<"----------Log in successfull--------";
+                        cout<<endl<<endl;
+
+
+                        cout<<" Details "<<endl;
+
+                        cout<<"Username: "+name<<endl;
+                        cout<<"Password: "+pass<<endl;
+                        cout<<"Age: "+age<<endl;
+                       } 
+                       else{
+                        cout<<endl<<endl;
+                        cout<<"Incorrect credentials"<<endl;
+                        cout<<"|     1. Press 2 to Login             |"<<endl;
+                        cout<<"|     2. Press 3 to change password   |"<<endl;
+                        
+                        break;
+                       }
+                    }
+                }
+                break;
+            }
+
+            case 3:{
+                i = 0;
+                cout<<"--------------change password--------------"<<endl;
+
+                ifstream of0;
+                of0.open("file.txt");
+                cout<<"Enter the old password:- ";
+                cin>>old;
+                if(of0.is_open()){
+                    while(!of0.eof()){
+                        while(getline(of0,text)){
+                            istringstream iss(text);
+                            iss>>word1;
+                            cp[i]=word1;
+                            i++;
+                        }
+                        if(old==cp[1]){
+                            of0.close();
+
+                            ofstream of1;
+                            of1.open("file.txt");
+                            if(of1.is_open()){
+                                cout<<"Enter your new password:- ";
+                                cin>>password1;
+                                cout<<"Enter your password again:- ";
+                                cin>>password2;
+
+                                if(password1==password2){
+                                    of1<<cp[0]<<"\n";
+                                    of1<<password1;
+                                    cout<<"Password change successfully!"<<endl;
+                                }else{
+                                    of1<<cp[0]<<"\n";
+                                    of1<<old;
+                                    cout<<"password do not match!"<<endl;
+                                }
+                            }else{
+                                cout<<"Please enter a valid password"<<endl;
+                                break;
+                            }
+                        }
+                    }
+                }
+                break;
+            }
+
+            case 4:
+            {
+                cout<<"_____________Thank you !________________"<<endl;
+                break;
+            }
+            default:
+           cout<<"Enter a valid choice"<<endl;
+           break;
         }
 
-    }while(1);
-   
+    }while(a!=4);
+   return 0;
 }
